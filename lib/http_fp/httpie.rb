79c2dfdb620fc1b1c86209f817fad3e5ece67834
Martin Chabot <chabotm@gmail.com>
2017-03-24 22:04:39 -0400

    header to httpie

--- lib/http_fp/httpie.rb
+++ lib/http_fp/httpie.rb
@@ -7,11 +7,13 @@ module HttpFp::Httpie
   mattr_accessor :print_curl, :req
 
   @@req = -> req {
-    first_part = %{http #{req[:method]} '#{HttpFp::to_uri.(req).to_s}' #{req[:header].map(&@@header_to_curl).join(" ")}}
+    first_part = %{http #{req[:method]} '#{HttpFp::to_uri.(req).to_s}' #{req[:header].map(&@@header_to_httpie).join(" ")}}
     if req[:body] && !req[:body].empty?
       %{echo $'#{req[:body].gsub("'", "\'")}' |\\\n#{first_part}}
     else
       first_part
     end
   }
+  @@header_to_httpie = -> a {
+   "\\\n    '#{a[0]}: #{a[1]}'"}
 end
